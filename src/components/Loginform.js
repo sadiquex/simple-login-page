3bdd46eb97b3dc808a3597bf8624c59bddeeb87f
sadiquex <abubakasaddik1@gmail.com>
2022-11-27 11:41:49 +0000

    added popup msg

--- src/components/Loginform.js
+++ src/components/Loginform.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Loginform.css";
 
 const Loginform = () => {
+  const [message, showPopup] = useState("hide");
+
+  const popup = () => {
+    showPopup("login-popup");
+    setTimeout(() => {
+      showPopup("hide");
+    }, 3000);
+  };
+
   return (
     <div className="container">
       <h1>login</h1>
       <input type="text" placeholder="username" />
       <input type="password" placeholder="password" />
-      <button className="btn">Log In</button>
-      <h2 className="or">Or login using</h2>
+      <button className="btn" onClick={popup}>
+        Log In
+      </button>
+      <a href="#" className="or">
+        Or login using
+      </a>
       <div className="use-accounts">
         <button className="google">
           <i class="fa fa-envelope" aria-hidden="true"></i>
@@ -16,6 +29,10 @@ const Loginform = () => {
         <button className="google">
           <i class="fa fa-address-book" aria-hidden="true"></i>
         </button>
+      </div>
+
+      <div className={message}>
+        <h2>Thank you for loggin in</h2>
       </div>
     </div>
   );
